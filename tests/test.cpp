96a81bd8fcefe67720a93c82d6208497b65b0dc1
Bryan Hu <bryan.hu.2020@gmail.com>
2021-01-01 14:21:35 -0800

    Epicify

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,12 +1,44 @@
 /* It sure is a sunny day! */
 #include <iostream>
 #include <regex>
+#include <cstring>
+#include <new>
 
 
 void hello_world(const char* name) {
     /* Hello there. */
     std::cout << "Hello, " << name << std::endl;
 }
+
+
+class Greeter {
+public:
+    Greeter(char* name) {
+        try {
+            _name = new char[strlen(name) + 1];
+        }
+        catch (std::bad_alloc exception) {
+            std::cout << exception.what() << std::endl;
+        }
+        strcpy(_name, name);
+    };
+    ~Greeter() {
+        delete [] _name;
+    };
+    const char * get_name() const {return _name;};
+    void set_name(char * new_name) {
+        try {
+            _name = new char[strlen(new_name) + 1];
+        }
+        catch (std::bad_alloc exception) {
+            std::cout << exception.what() << std::endl;
+        }
+        strcpy(_name, new_name);
+    };
+    void greet() const {hello_world(_name);}
+private:
+    char* _name;
+};
 
 
 std::regex SOME_RE("T[hat]{3}Xliner");
