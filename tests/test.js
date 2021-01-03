03d2c1931f08d51303d65dbf4e43325a9c0f49e7
KomodoKode <71739129+KomodoKode@users.noreply.github.com>
2021-01-02 16:03:27 -0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -9,10 +9,10 @@ var SOME_RE = new RegExp("T[hat]{3}Xliner");
 var argv = process.argv.slice(2);
 
 if (require.main === module) {
-  if !( argv[1].match(SOME_RE ) {
+  if !( argv[1].match(SOME_RE) ) {
     hello_world(argv[1]);
   } else {
     console.log("You are cool!"); // haha, vanity
-    // NOTE : Reduce the vanity
+    // NOTE: Reduce the vanity
   };
 };
