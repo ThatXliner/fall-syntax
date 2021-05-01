7cf893fe711ba3333e87c8a7d3fc4e928011b98c
Bryan Hu <bryan.hu.2020@gmail.com>
2021-04-30 20:09:24 -0700

    better

--- tests/test.js
+++ tests/test.js
@@ -9,10 +9,19 @@ var SOME_RE = new RegExp("T[hat]{3}Xliner");
 var argv = process.argv.slice(2);
 
 if (require.main === module) {
-  if !( argv[1].match(SOME_RE) ) {
+  if (!argv[1].match(SOME_RE)) {
     hello_world(argv[1]);
   } else {
     console.log("You are cool!"); // haha, vanity
     // NOTE: Reduce the vanity
-  };
-};
+  }
+}
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+const rect = new Rectangle();
+rect.width;
+e.e();
